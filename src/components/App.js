14c90c48ad5189d00ab5e3b855f91160c79471d1
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-27 15:51:00 +0200

    Show message after form submit and clear inputs

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     password2: '',
     accept: false,
     correct: false,
+    confirmMessage: '',
 
     errors: {
       username: false,
@@ -25,7 +26,7 @@ class App extends Component {
     emailError: 'Email is not valid',
     passwordError: 'Password must be at least 6 characters',
     password2Error: 'Password confirm is not correct',
-    acceptError: 'You must confirm terms'
+    acceptError: 'You must accept terms',
   }
 
   handleChange = e => {
@@ -51,24 +52,24 @@ class App extends Component {
     let correct = false;
 
     // Validate username
-    if (this.state.username.length < 3) {
+    if (this.state.username.length > 2) {
       username = true;
     }
     // Validate email
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(String(this.state.email).toLowerCase())) {
+    if (re.test(String(this.state.email).toLowerCase())) {
       email = true;
     }
     // Validate password
-    if (this.state.password.length < 5) {
+    if (this.state.password.length > 5) {
       password = true;
     }
     // Validate password2
-    if (this.state.password2 !== this.state.password) {
+    if (this.state.password2 === this.state.password && this.state.password2 !== '') {
       password2 = true;
     }
     // Validate accept
-    if (!this.state.accept) {
+    if (this.state.accept) {
       accept = true;
     }
     // Validate form
@@ -83,77 +84,101 @@ class App extends Component {
     e.preventDefault()
     const validate = this.validateForm();
 
-    this.setState({
-      errors: {
-        username: validate.username,
-        email: validate.email,
-        password: validate.password,
-        password2: validate.password2,
-        accept: validate.accept,
-      }
-    })
+    if (validate.correct) {
+      this.setState({
+        username: '',
+        email: '',
+        password: '',
+        password2: '',
+        accept: false,
+        confirmMessage: 'Registration Completed',
+
+        errors: {
+          username: false,
+          email: false,
+          password: false,
+          password2: false,
+          accept: false,
+          correct: false,
+        }
+      })
+    } else {
+      this.setState({
+        correct: validate.correct,
+        errors: {
+          username: !validate.username,
+          email: !validate.email,
+          password: !validate.password,
+          password2: !validate.password2,
+          accept: !validate.accept,
+        }
+      })
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} noValidate>
-        <h2>Register Form</h2>
-        <InputField
-          type='text'
-          name='username'
-          title='Username'
-          placeholder='Enter username...'
-          value={this.state.username}
-          change={this.handleChange}
-          isError={this.state.errors.username}
-        />
-        {this.state.errors.username ? <span className='error'>{this.errorMessages.usernameError}</span> : null}
-        <br />
-        <InputField
-          type='email'
-          name='email'
-          title='Email'
-          placeholder='Enter email...'
-          value={this.state.email}
-          change={this.handleChange}
-          isError={this.state.errors.email}
-        />
-        {this.state.errors.email ? <span className='error'>{this.errorMessages.emailError}</span> : null}
-        <br />
-        <InputField
-          type='password'
-          name='password'
-          title='Password'
-          placeholder='Enter password...'
-          value={this.state.password}
-          change={this.handleChange}
-          isError={this.state.errors.password}
-        />
-        {this.state.errors.password ? <span className='error'>{this.errorMessages.passwordError}</span> : null}
-        <br />
-        <InputField
-          type='password'
-          name='password2'
-          title='Confirm password'
-          placeholder='Enter password again...'
-          value={this.state.password2}
-          change={this.handleChange}
-          isError={this.state.errors.password2}
-        />
-        {this.state.errors.password2 ? <span className='error'>{this.errorMessages.password2Error}</span> : null}
-        <br />
-        <InputField
-          type='checkbox'
-          name='accept'
-          title='Accept terms'
-          checked={this.state.accept}
-          change={this.handleChange}
-          isError={this.state.errors.accept}
-        />
-        {this.state.errors.accept ? <span className='error'>{this.errorMessages.acceptError}</span> : null}
-        <br />
-        <button>Submit</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit} noValidate>
+          <h2>Register Form</h2>
+          <InputField
+            type='text'
+            name='username'
+            title='Username'
+            placeholder='Enter username...'
+            value={this.state.username}
+            change={this.handleChange}
+            isError={this.state.errors.username}
+          />
+          {this.state.errors.username ? <span className='error'>{this.errorMessages.usernameError}</span> : null}
+          <br />
+          <InputField
+            type='email'
+            name='email'
+            title='Email'
+            placeholder='Enter email...'
+            value={this.state.email}
+            change={this.handleChange}
+            isError={this.state.errors.email}
+          />
+          {this.state.errors.email ? <span className='error'>{this.errorMessages.emailError}</span> : null}
+          <br />
+          <InputField
+            type='password'
+            name='password'
+            title='Password'
+            placeholder='Enter password...'
+            value={this.state.password}
+            change={this.handleChange}
+            isError={this.state.errors.password}
+          />
+          {this.state.errors.password ? <span className='error'>{this.errorMessages.passwordError}</span> : null}
+          <br />
+          <InputField
+            type='password'
+            name='password2'
+            title='Confirm password'
+            placeholder='Enter password again...'
+            value={this.state.password2}
+            change={this.handleChange}
+            isError={this.state.errors.password2}
+          />
+          {this.state.errors.password2 ? <span className='error'>{this.errorMessages.password2Error}</span> : null}
+          <br />
+          <InputField
+            type='checkbox'
+            name='accept'
+            title='Accept terms'
+            checked={this.state.accept}
+            change={this.handleChange}
+            isError={this.state.errors.accept}
+          />
+          {this.state.errors.accept ? <span className='error'>{this.errorMessages.acceptError}</span> : null}
+          <br />
+          <button>Submit</button>
+        </form>
+        {this.state.confirmMessage && <h4>Registration Completed</h4>}
+      </div>
     );
   };
 };
