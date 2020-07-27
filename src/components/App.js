1c5c4dd512d1215efcdcddf53ed4c7278de9d896
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-27 13:06:16 +0200

    Create base components structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import InputField from './InputField/InputField'
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <form>
+        <h2>Register Form</h2>
+        <InputField />
+        <InputField />
+        <InputField />
+        <InputField />
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
 
 export default App;
