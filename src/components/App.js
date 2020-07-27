513d65808930be47b74de22844a240829819a811
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-27 14:29:49 +0200

    Add error messages

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,26 @@ class App extends Component {
     email: '',
     password: '',
     password2: '',
-    accept: false
+    accept: false,
+
+    errors: {
+      username: true,
+      email: true,
+      password: true,
+      password2: true,
+      accept: true,
+    }
   };
 
-  handleChange = (e) => {
+  errorMessages = {
+    usernameError: 'Username must be at least 3 characters',
+    emailError: 'Email is not valid',
+    passwordError: 'Password must be at least 6 characters',
+    password2Error: 'Password confirm is not correct',
+    acceptError: 'You must confirm terms'
+  }
+
+  handleChange = e => {
     const input = e.target;
     const inputName = e.target.name;
     if (input.type === 'checkbox') {
@@ -25,9 +41,14 @@ class App extends Component {
     };
   };
 
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('Submit!')
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit} noValidate>
         <h2>Register Form</h2>
         <InputField
           type='text'
@@ -36,7 +57,10 @@ class App extends Component {
           placeholder='Enter username...'
           value={this.state.username}
           change={this.handleChange}
+          isError={this.state.errors.username}
         />
+        {this.state.errors.username ? <span className='error'>{this.errorMessages.usernameError}</span> : null}
+        <br />
         <InputField
           type='email'
           name='email'
@@ -44,7 +68,10 @@ class App extends Component {
           placeholder='Enter email...'
           value={this.state.email}
           change={this.handleChange}
+          isError={this.state.errors.email}
         />
+        {this.state.errors.email ? <span className='error'>{this.errorMessages.emailError}</span> : null}
+        <br />
         <InputField
           type='password'
           name='password'
@@ -52,7 +79,10 @@ class App extends Component {
           placeholder='Enter password...'
           value={this.state.password}
           change={this.handleChange}
+          isError={this.state.errors.password}
         />
+        {this.state.errors.password ? <span className='error'>{this.errorMessages.passwordError}</span> : null}
+        <br />
         <InputField
           type='password'
           name='password2'
@@ -60,14 +90,20 @@ class App extends Component {
           placeholder='Enter password again...'
           value={this.state.password2}
           change={this.handleChange}
+          isError={this.state.errors.password2}
         />
+        {this.state.errors.password2 ? <span className='error'>{this.errorMessages.password2Error}</span> : null}
+        <br />
         <InputField
           type='checkbox'
           name='accept'
           title='Accept terms'
           checked={this.state.accept}
           change={this.handleChange}
+          isError={this.state.errors.accept}
         />
+        {this.state.errors.accept ? <span className='error'>{this.errorMessages.acceptError}</span> : null}
+        <br />
         <button>Submit</button>
       </form>
     );
